bd5c752be9d8a942d6819c512144b758908399f7
Joel Ibaceta <mail@joelibaceta.com>
2022-01-22 08:45:54 -0500

    including filters

--- components/product_item.js
+++ components/product_item.js
@@ -2,11 +2,11 @@ Vue.component('product-item', {
   props: ['product'],
   template: `
  
-      <div class="row no-gutters">
+      <div class="row">
         <div class="col-md-2">
           <img class="img-thumbnail" v-bind:src="product.image_url" alt="Card image cap">
         </div>
-        <div class="col-md-6"> 
+        <div class="col-md-6">
             <h5>{{ product.name }}</h5>
             <p>{{ product.description }}</p>
             <p>{{ product.price }}</p>
@@ -15,6 +15,7 @@ Vue.component('product-item', {
         <div class="col-md-4"> 
           <button type="button" class="btn btn-primary" @click="$emit('on-add-to-cart', product)">Agregar al carrito</button>
         </div>
+
       
       </div>
  
--- components/product_list.js
+++ components/product_list.js
@@ -3,7 +3,8 @@ Vue.component('product-list', {
     return { 
       products: [],
       loading: true,
-      token: localStorage.getItem('token')
+      token: localStorage.getItem('token'),
+      search: '',
     }
   },
   created: function () {
@@ -33,8 +34,36 @@ Vue.component('product-list', {
       </div>
     </div>
     <div v-else>
-      <div class="list-group">
-        <product-item v-for="product in products" v-bind:product="product"></product-item>
+      <div class="row">
+        <div class="col-12">
+          <div class="search">
+            <input type="text" class="form-control" placeholder="Buscar producto" v-model="search">
+          </div>
+        </div>
+      </div>
+      <hr/>
+      <ul class="nav nav-pills nav-fill">
+        <li class="nav-item">
+          <a class="nav-link active sm" aria-current="page" href="#">Alimentos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tecnologia</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Ropa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">Farmacia</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">Belleza</a>
+        </li>
+      </ul>
+      <hr/>
+      <div class="row">
+        <div class="col-12">
+          <product-item v-for="product in products" v-bind:product="product"></product-item>
+        </div>
       </div>
     </div>
   `,
